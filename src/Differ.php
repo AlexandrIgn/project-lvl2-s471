3ba849c0de62b2e987d6f5bec1cf0c8d74d75c29
AlexandrIgn <asignatyev@mail.ru>
2019-05-03 09:11:37 +0400

    complete 3 step

--- src/Differ.php
+++ src/Differ.php
@@ -3,13 +3,33 @@
 namespace DifferenceCalculator\Differ;
 
 use function Funct\Collection\union;
+use Symfony\Component\Yaml\Yaml;
 
 function genDiff($firstPathToFile, $secondPathToFile)
 {
-    $dataFirstFile = file_get_contents($firstPathToFile);
-    $dataSecondFile = file_get_contents($secondPathToFile);
-    $firstDecodeFile = json_decode($dataFirstFile, true);
-    $secondDecodeFile = json_decode($dataSecondFile, true);
+    if (getFileExtension($firstPathToFile) === 'json' && getFileExtension($secondPathToFile) === 'json') {
+        $dataFirstFile = file_get_contents($firstPathToFile);
+        $dataSecondFile = file_get_contents($secondPathToFile);
+        $firstDecodeFile = json_decode($dataFirstFile, true);
+        $secondDecodeFile = json_decode($dataSecondFile, true);
+        return getParsedFile($firstDecodeFile, $secondDecodeFile);
+    } elseif (getFileExtension($firstPathToFile) === 'yml' && getFileExtension($secondPathToFile) === 'yml') {
+        $dataFirstFile = file_get_contents($firstPathToFile);
+        $dataSecondFile = file_get_contents($secondPathToFile);
+        $firstDecodeFile = Yaml::parse($dataFirstFile);
+        $secondDecodeFile = Yaml::parse($dataSecondFile);
+        return getParsedFile($firstDecodeFile, $secondDecodeFile);
+    }
+    
+}
+
+function getFileExtension($pathToFile)
+{
+    return pathinfo($pathToFile, PATHINFO_EXTENSION);
+}
+
+function getParsedFile($firstDecodeFile, $secondDecodeFile)
+{
     $unionFirstAndSecondDecodeFiles = union($firstDecodeFile, $secondDecodeFile);
     $result = [];
     foreach ($unionFirstAndSecondDecodeFiles as $key => $value) {
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -7,11 +7,19 @@ use PHPUnit\Framework\TestCase;
 
 class DifferTest extends TestCase
 {
-    public function testGenDiff()
+    public function testGenDiffJson()
     {
         $pathToFirstFile = 'tests/JsonFiles/before.json';
         $pathToSecondFile = 'tests/JsonFiles/after.json';
         $expected = file_get_contents('tests/JsonFiles/expected');
+        $this->assertEquals($expected, genDiff($pathToFirstFile, $pathToSecondFile));
+    }
+
+    public function testGenDiffYml()
+    {
+        $pathToFirstFile = 'tests/YamlFiles/before.yml';
+        $pathToSecondFile = 'tests/YamlFiles/after.yml';
+        $expected = file_get_contents('tests/YamlFiles/expected');
         $this->assertEquals($expected, genDiff($pathToFirstFile, $pathToSecondFile));
     }
 }
